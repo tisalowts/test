56c9afce3782cce2e793e9e60eab45d36fc971cf
15865279708@163.com <15865279708@163.com>
2018-07-06 17:48:15 +0800

    new

--- oop/111.py
+++ oop/111.py
@@ -1,1 +1,26 @@
-iii安抚安抚安抚啊
+class Person():
+    name = "init"
+    age = 0
+    __age = 0
+    _name = "halfname"
+
+    def sleep(self):
+        print("sleep.....")
+
+    def Work(self):
+        print("Get some money")
+
+
+class Teacher(Person):
+    name = "zhaolinger"
+    # def Work(self):
+    # Person.Work(self)
+    # print("teacher students")
+    # def Work(self):
+    # self.Work_01()
+    # Person.Work(self)
+
+
+t = Teacher()
+print(t.Work())
+print(t.sleep())
